a939e631eb0c2e0d92824ca41311dd6b822c4d26
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-07-28 23:53:27 +0200

    require fixed

--- files/install.php
+++ files/install.php
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-require_once(__DIR__."/../../../vendor/autoload.php");
+require_once(__DIR__."/../../../autoload.php");
 ?>
 <!DOCTYPE html>
 <html>
